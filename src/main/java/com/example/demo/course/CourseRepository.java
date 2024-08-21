2b2fd36604d096e60d5d79aef2a5a51e7ea90f53
racchittt <112688112+racchittt@users.noreply.github.com>
2024-08-22 00:41:40 +0530

    data access layer (jpa repository)

---
+++ src/main/java/com/example/demo/course/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+// public interface CourseRepository extends JpaRepository <T (type), Long (id)>{}
+public interface CourseRepository extends JpaRepository <Course, Long>{
+    
+}
--- src/main/java/com/example/demo/course/CourseService.java
+++ src/main/java/com/example/demo/course/CourseService.java
@@ -1,17 +1,19 @@
 package com.example.demo.course;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service //adding a bean
 public class CourseService {
+	private final CourseRepository courseRepository;
+
+	@Autowired
+	public CourseService(CourseRepository courseRepository) {
+		this.courseRepository = courseRepository;
+	}
+
     public List<Course> getCourses(){
-		return List.of(
-			new Course(
-				1L,
-				"Introduction to Computer Architecture",
-				"CS 102",
-				"This course provides a basic introduction to the architecture and algorithms of computer systems"
-			)
-		);
+		return courseRepository.findAll();
 	}
 }
