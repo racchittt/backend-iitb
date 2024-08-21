a1aacf551eb78332798dce3a1d9a95e4bcac78da
racchittt <112688112+racchittt@users.noreply.github.com>
2024-08-22 00:59:28 +0530

    saving courses logic

---
+++ src/main/java/com/example/demo/course/CourseConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.course;
+
+import java.util.List;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CourseConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(CourseRepository repository) {
+        return args -> {
+            Course CA = new Course(
+                    "Introduction to Computer Architecture",
+                    "CS 102",
+                    "This course provides a basic introduction to the architecture and algorithms of computer systems");
+            Course DSA = new Course(
+                    "Data Structures and Algorithms",
+                    "CS 201",
+                    "This course covers fundamental data structures and algorithms with a focus on efficiency and performance");
+            repository.saveAll(List.of(CA,DSA));
+
+        };
+    }
+}
